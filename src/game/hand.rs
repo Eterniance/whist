4368a698df43fd611a7eea923df017aafe9d5888
Eterniance <francois.kinet@gmail.com>
2026-01-04 11:19:00 +0100

    removed async
    We might create a 'sync' feature in the future

--- src/game/hand.rs
+++ src/game/hand.rs
@@ -3,40 +3,22 @@ use super::{
     players::Contractors,
     rules::{Contract, ContractorsKind},
 };
-use crate::{
-    game::players::{PlayerId, Players},
-    gamemodes::{Score, TOTAL_TRICKS},
-};
-use async_trait::async_trait;
-use std::{collections::HashMap, ops::RangeInclusive, sync::Arc};
+use crate::gamemodes::{Score, TOTAL_TRICKS};
+use std::rc::Rc;
 use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum InputError {
     #[error("invalid input: {0}")]
     InvalidInput(String),
-}
-
-#[cfg_attr(target_arch = "wasm32", async_trait(?Send))]
-#[cfg_attr(not(target_arch = "wasm32"), async_trait)]
-pub trait Requester {
-    async fn ask_bid(&self, range: RangeInclusive<i16>) -> Result<i16, InputError>;
-    async fn ask_name(&self) -> Result<String, InputError>;
-    async fn pick_names(
-        &self,
-        names_list: Vec<String>,
-        names_number: usize,
-    ) -> Result<Vec<String>, InputError>;
-
-    async fn pick_name(&self, names_list: Vec<String>) -> Option<String> {
-        self.pick_names(names_list, 1).await.ok()?.first().cloned()
-    }
+    #[error("The score sum cannot be zero")]
+    WrongScore,
 }
 
 #[derive(Debug)]
 pub struct Hand {
     pub contractors: Contractors,
-    contract: Arc<Contract>,
+    contract: Rc<Contract>,
     bid: Option<i16>,
     tricks: i16,
 }
@@ -95,7 +77,7 @@ pub enum InputRequest {
 
 #[derive(Debug)]
 pub struct HandBuilder {
-    contract: Arc<Contract>,
+    contract: Rc<Contract>,
     contractors: Option<Contractors>,
     bid: Option<i16>,
     tricks: i16,
@@ -103,7 +85,7 @@ pub struct HandBuilder {
 
 impl HandBuilder {
     #[must_use]
-    pub const fn new(contract: Arc<Contract>) -> Self {
+    pub const fn new(contract: Rc<Contract>) -> Self {
         Self {
             contract,
             contractors: None,
@@ -215,188 +197,5 @@ impl HandBuilder {
             bid: self.bid,
             tricks: self.tricks,
         })
-    }
-}
-
-/// Interactively builds a `Hand` by requesting missing inputs from a requester.
-///
-/// The function drives a `HandBuilder` until all required information
-/// (contractors, bid) is provided, using the given `Requester` to obtain user
-/// input asynchronously.
-///
-/// # Errors
-///
-/// Returns an error if an invalid contractors selection or bid is provided,
-/// or if the requester fails when selecting multiple names.
-///
-/// # Panics
-///
-/// Panics if a selected player name cannot be resolved to a player ID,
-/// which assumes that `players` has been properly initialized and kept
-/// consistent with the requester.
-pub async fn build_hand<R: Requester + Sync + Send>(
-    contract: Arc<Contract>,
-    players: Arc<Players>,
-    requester: Arc<R>,
-) -> Result<Hand, GameError> {
-    let mut b = HandBuilder::new(contract);
-
-    loop {
-        match b.next_request() {
-            InputRequest::ContractorsSolo => {
-                let list = players.names();
-                if let Some(name) = requester.pick_name(list).await {
-                    let id = players.get_id(&name).expect("Players is initialized");
-                    b.set_contractors(Contractors::Solo(id))?;
-                }
-            }
-            InputRequest::ContractorsTeam => {
-                let list = players.names();
-                let names = requester.pick_names(list, 2).await?;
-                let id1 = players.get_id(&names[0]).expect("Players is initialized");
-                let id2 = players.get_id(&names[1]).expect("Players is initialized");
-                b.set_contractors(Contractors::Team(id1, id2))?;
-            }
-            InputRequest::ContractorsOther => todo!(),
-            InputRequest::Bid { min, max } => {
-                let bid = requester.ask_bid(min..=max).await?;
-                b.set_bid(bid)?;
-            }
-            InputRequest::Done => {
-                let hand = b.build()?;
-                return Ok(hand);
-            }
-            InputRequest::Cancel => todo!(),
-        }
-    }
-}
-
-#[derive(Debug, Clone)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-pub struct HandRecap {
-    pub gamemode_name: String,
-    pub scores: HashMap<PlayerId, i16>,
-    pub tricks: i16,
-    pub contractors: Contractors,
-    pub bid: Option<i16>,
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::{
-        game::{
-            players::PlayerId,
-            rules::{GameRules, select_rules},
-        },
-        gamemodes::TOTAL_TRICKS,
-    };
-
-    use super::*;
-
-    #[test]
-    fn build_hand() {
-        let scorables: Vec<_> = select_rules(&GameRules::Dutch);
-        let emballage = Arc::new(scorables[0].clone());
-
-        let mut builder = HandBuilder::new(emballage);
-
-        assert!(matches!(
-            builder.next_request(),
-            InputRequest::ContractorsTeam
-        ));
-        let player1 = PlayerId::new(0);
-        let player2 = PlayerId::new(1);
-        let c = Contractors::Team(player1.clone(), player2.clone());
-        builder.set_contractors(c).unwrap();
-
-        let (min, max) = match builder.next_request() {
-            InputRequest::Bid { min, max } => (min, max),
-            other => panic!("Expected Bid request, got {other:?}"),
-        };
-        assert_eq!(min, 8);
-        assert_eq!(max, TOTAL_TRICKS);
-
-        let bid = 10;
-        assert!(bid <= max);
-        builder.set_bid(bid).unwrap();
-
-        assert!(matches!(builder.next_request(), InputRequest::Done));
-
-        let hand = builder.build().unwrap();
-
-        match hand.contractors {
-            Contractors::Team(a, b) => {
-                // Keep these checks aligned with the `c` above.
-                assert_eq!(a, player1);
-                assert_eq!(b, player2);
-            }
-            _ => panic!("Expected Contractors::Team"),
-        }
-
-        assert_eq!(hand.bid, Some(bid));
-    }
-
-    #[test]
-    fn build_hand_picolo() {
-        let scorables = select_rules(&GameRules::French);
-        let picolo = Arc::new(scorables[2].clone());
-
-        let mut builder = HandBuilder::new(picolo);
-
-        assert!(matches!(
-            builder.next_request(),
-            InputRequest::ContractorsSolo
-        ));
-
-        let player = PlayerId::new(0);
-        builder
-            .set_contractors(Contractors::Solo(player.clone()))
-            .unwrap();
-
-        let bid = 4;
-        builder.set_bid(bid).unwrap();
-
-        assert!(builder.bid.is_none());
-
-        assert!(matches!(builder.next_request(), InputRequest::Done));
-
-        let hand = builder.build().unwrap();
-
-        match hand.contractors {
-            Contractors::Solo(p) => assert_eq!(p, player),
-            _ => panic!("Expected Contractors::Solo"),
-        }
-
-        assert!(hand.bid.is_none());
-    }
-
-    #[test]
-    fn build_hand_failures() {
-        let scorables = select_rules(&GameRules::Dutch);
-        let emballage = Arc::new(scorables[0].clone());
-
-        let builder = HandBuilder::new(Arc::clone(&emballage));
-        let err = builder.build().unwrap_err();
-        assert!(matches!(err, GameError::HandBuildError(_)));
-
-        let mut builder = HandBuilder::new(Arc::clone(&emballage));
-
-        let solo_player = PlayerId::new(0);
-        let err = builder
-            .set_contractors(Contractors::Solo(solo_player))
-            .unwrap_err();
-
-        assert!(matches!(err, GameError::HandBuildError(_)));
-
-        let mut builder = HandBuilder::new(emballage);
-
-        let p1 = PlayerId::new(0);
-        let p2 = PlayerId::new(1);
-        builder.set_contractors(Contractors::Team(p1, p2)).unwrap();
-
-        assert!(matches!(builder.next_request(), InputRequest::Bid { .. }));
-
-        let err = builder.build().unwrap_err();
-        assert!(matches!(err, GameError::HandBuildError(_)));
     }
 }
